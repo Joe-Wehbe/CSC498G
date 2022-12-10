7dfac23701ed8dbb0407a1dc97d0850ea0c5eb6f
Joe-Wehbe <joewehbe@yahoo.com>
2022-12-10 15:10:41 +0200

    Coded AddDistanceActivity

--- MyCar/app/src/main/java/com/mycar/SignInActivity.java
+++ MyCar/app/src/main/java/com/mycar/SignInActivity.java
@@ -22,7 +22,7 @@ public class SignInActivity extends AppCompatActivity {
 
     public void goToSignUp(View v){
 
-        Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
+        Intent intent = new Intent(getApplicationContext(), AddDistanceActivity.class);
         startActivity(intent);
 
     }
