1a98bf97275e19fa7aa2fba232997f46b12f53e3
Joe-Wehbe <joewehbe@yahoo.com>
2022-12-11 23:17:41 +0200

    Coded signin backend

--- MyCar/app/src/main/java/com/mycar/SignInActivity.java
+++ MyCar/app/src/main/java/com/mycar/SignInActivity.java
@@ -1,15 +1,33 @@
 package com.mycar;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private final String URL = "http://10.31.210.7/MyCar/signIn.php";
+    private EditText etEmail, etPassword;
+    private String email, password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +36,66 @@ public class SignInActivity extends AppCompatActivity {
 
         Objects.requireNonNull(getSupportActionBar()).setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.dark_gray)));
         getSupportActionBar().setTitle("Sign In");
+
+        email = password = "";
+        etEmail = findViewById(R.id.email_address1);
+        etPassword = findViewById(R.id.password1);
     }
+
+    public void login(View view){
+        email = etEmail.getText().toString().trim();
+        password = etPassword.getText().toString().trim();
+
+        //.makeText(SignInActivity.this, "test0", Toast.LENGTH_SHORT).show();
+
+        if(!email.equals("") && !password.equals("")){
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+
+                    if (response.trim().equals("success")) {
+                        Toast.makeText(SignInActivity.this, "test3", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(SignInActivity.this, NoCarsActivity.class);
+                        startActivity(intent);
+                        //finish();
+                    } else if (response.trim().equals("failure")) {
+                        Toast.makeText(SignInActivity.this, "Invalid email or password", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                    Toast.makeText(SignInActivity.this, error.toString().trim(), Toast.LENGTH_SHORT).show();
+                }
+            }){
+                @Nullable
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+
+                    Map<String, String> data = new HashMap<>();
+                    data.put("email", email);
+                    data.put("password", password);
+                    return data;
+                }
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+            requestQueue.add(stringRequest);
+        }else{
+            Toast.makeText(this, "Fields cannot be empty!", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
+    public void signIn(View v){
+        login(v);
+    }
+
 
     public void goToSignUp(View v){
 
-        Intent intent = new Intent(getApplicationContext(), NoCarsActivity.class);
+        Intent intent = new Intent(getApplicationContext(), SignUpActivity.class);
         startActivity(intent);
 
     }
--- MyCar/app/src/main/java/com/mycar/SignUpActivity.java
+++ MyCar/app/src/main/java/com/mycar/SignUpActivity.java
@@ -1,15 +1,38 @@
 package com.mycar;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
 import java.util.Objects;
 
 public class SignUpActivity extends AppCompatActivity {
 
-
+    private EditText etFirstName, etLastName, etCountry, etEmail, etPassword, etConfirm;
+    //private TextView tvStatus;
+    private ImageView btnRegister;
+    private String URL = "http://10.31.210.7/MyCar/signUp.php";
+    private String first_name, last_name, country, email, password, confirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +41,73 @@ public class SignUpActivity extends AppCompatActivity {
 
         Objects.requireNonNull(getSupportActionBar()).setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.dark_gray)));
         getSupportActionBar().setTitle("Sign Up");
+
+        etFirstName = findViewById(R.id.first_name);
+        etLastName = findViewById(R.id.last_name);
+        etCountry = findViewById(R.id.country);
+        etEmail = findViewById(R.id.email_address1);
+        etPassword = findViewById(R.id.password1);
+        etConfirm = findViewById(R.id.confirm_your_password);
+        btnRegister = findViewById(R.id.sign_up_button);
+
+        first_name = last_name = country = email = password = confirm = "";
+
+    }
+
+    public void signUp(View v){
+        first_name = etFirstName.getText().toString().trim();
+        last_name = etLastName.getText().toString().trim();
+        country = etCountry.getText().toString().trim();
+        email = etEmail.getText().toString().trim();
+        password = etPassword.getText().toString().trim();
+        confirm = etConfirm.getText().toString().trim();
+
+        if(!password.equals(confirm)){
+            Toast.makeText(this, "Password mismatch", Toast.LENGTH_SHORT).show();
+
+        }else if(!first_name.equals("")
+        && !last_name.equals("")
+        && !country.equals("")
+        && !email.equals("")
+        && !password.equals("")){
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+
+                    Toast.makeText(SignUpActivity.this, response , Toast.LENGTH_SHORT).show();
+
+                    if (response.trim().equals("success")) {
+                        Intent intent = new Intent(SignUpActivity.this, NoCarsActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else if (response.trim().equals("failure")) {
+                        Toast.makeText(SignUpActivity.this, "Something went wrong", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(getApplicationContext(), error.toString().trim(), Toast.LENGTH_SHORT).show();
+                }
+            }){
+                @Nullable
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> data = new HashMap<>();
+                    data.put("firstName", first_name);
+                    data.put("lastName", last_name);
+                    data.put("country", country);
+                    data.put("email", email);
+                    data.put("password", password);
+                    return data;
+                }
+            };
+            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+            requestQueue.add(stringRequest);
+
+
+        }
 
     }
 
